a6020c60f524a159ce58ae5da39f5f35271af926
icarranzani <icarranzani@netrivals.com>
2022-11-06 11:07:42 +0100

    New packages and configurations has been installed

---
+++ frontend/webpack.config.js
@@ -0,0 +1,48 @@
+module.exports = () => {
+  return {
+    entry: './src/index.tsx',
+    module: {
+      rules: [
+        {
+          test: /\.(ts|tsx)$/,
+          exclude: /node_modules/,
+          use: 'ts-loader',
+        },
+        {
+          test: /\.css$/,
+          use: [
+            {
+              loader: 'css-loader',
+            },
+          ],
+        },
+        {
+          test: /\.(sa|sc)ss$/,
+          use: [
+            {
+              loader: 'css-loader',
+            },
+            {
+              loader: 'sass-loader',
+            },
+          ],
+        },
+        {
+          test: /\.(woff|woff2|eot|ttf|png|jpe?g|gif|svg)$/,
+          include: [path.resolve(__dirname, './public')],
+          type: 'file-loader',
+          generator: {
+            filename: '[path][name][ext]',
+          },
+        },
+      ],
+    },
+    resolve: {
+      alias: {
+        '@': path.resolve(__dirname, './src/'),
+        '@public': path.resolve(__dirname, './public/'),
+      },
+      extensions: ['.tsx', '.ts'],
+    },
+  }
+}
