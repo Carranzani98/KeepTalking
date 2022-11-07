59097702f1698472cedf25bb4313edd3e73b52eb
icarranzani <isabella.carranzani@lengow.com>
2022-11-07 23:02:43 +0100

    New model for the user register and the db table has been modified

--- backend/app/Models/User.php
+++ backend/app/Models/User.php
@@ -7,8 +7,9 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
+use PHPOpenSourceSaver\JWTAuth\Contracts\JWTSubject;
 
-class User extends Authenticatable
+class User extends Authenticatable implements JWTSubject
 {
     use HasApiTokens, HasFactory, Notifiable;
 
@@ -18,9 +19,13 @@ class User extends Authenticatable
      * @var array<int, string>
      */
     protected $fillable = [
+        'id',
         'name',
+        'surname',
+        'country',
+        'birthday', 
         'email',
-        'password',
+        'description'
     ];
 
     /**
@@ -41,4 +46,38 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+     /**
+     * Get the identifier that will be stored in the subject claim of the JWT.
+     *
+     * @return mixed
+     */
+    public function getJWTIdentifier()
+    {
+        return $this->getKey();
+    }
+
+    /**
+     * Return a key value array, containing any custom claims to be added to the JWT.
+     *
+     * @return array
+     */
+    public function getJWTCustomClaims()
+    {
+        return [];
+    }
+
+    public function languages()
+    {
+        return $this->belongsToMany(Language::class, 'language_user', 'user_id', 'language_code');
+
+    }
+
+    public function languagesToLearn()
+    {
+        return $this->belongsToMany(Language::class, 'language_to_learn_user', 'user_id', 'language_code');
+
+    }
+
+
 }
