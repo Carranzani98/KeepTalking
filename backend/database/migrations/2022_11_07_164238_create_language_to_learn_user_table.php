fc35d531246e6ef32fc69b9300b73afd4d3aa7eb
icarranzani <isabella.carranzani@lengow.com>
2022-11-07 23:09:20 +0100

    New relation table between users and languages
    clear

---
+++ backend/database/migrations/2022_11_07_164238_create_language_to_learn_user_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('language_to_learn_user', function (Blueprint $table) {
+            $table->integer('user_id');
+            $table->string('language_code');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('language_to_learn_user');
+    }
+};
