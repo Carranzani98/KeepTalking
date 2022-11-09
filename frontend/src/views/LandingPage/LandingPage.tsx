74e2d087df6d6f083692e450c5b180e0075b6b08
icarranzani <isabella.carranzani@lengow.com>
2022-11-09 15:43:30 +0100

    New views for the second TFM stage have been added

---
+++ frontend/src/views/LandingPage/LandingPage.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+import { Box } from '@mantine/core'
+
+const LandingPage = () => {
+  return <Box>LandingPage in progress</Box>
+}
+
+export default LandingPage
---
+++ frontend/src/views/UsersList/UsersList.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+import { Box } from '@mantine/core'
+
+const UsersList = () => {
+  return <Box>UserList in progress</Box>
+}
+
+export default UsersList
