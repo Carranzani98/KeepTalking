a7ba37bb5b58c7a548a1d265f4d2803d69725e5c
Carranzani98 <icabo1598@gmail.com>
2023-01-05 18:08:56 +0100

    New favicon

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,16 @@
 import React from 'react'
 
 import { MantineProvider } from '@mantine/core'
+import { useFavicon } from '@mantine/hooks'
 import { ModalsProvider } from '@mantine/modals'
 import { NotificationsProvider } from '@mantine/notifications'
 
+import favicon from './icon/favicon.ico'
 import RoutesIndex from './views/RoutesIndex'
 
 const App = () => {
+  useFavicon(favicon)
+
   return (
     <MantineProvider>
       <ModalsProvider>
